d7b83c3490eecc0920e844f42f2f643d6ac00ccd
cchand3 <122644651+cchand3@users.noreply.github.com>
2023-11-12 19:57:00 -0500

    final commit

--- server.js
+++ server.js
@@ -1,34 +1,54 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
-const PORT = 8080;
+const port = 3000;
+const apiRouter = require('./routes/apiRouter');
+const bodyParser = require('body-parser'); // Ensure JSON parsing with this inclusion
+const users = require('./fakeUsers.json'); // Import user data
+
+// Set the view engine
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-app.use(express.urlencoded({ extended: true }));
-const users = require("./data/fakeUsers.json");
+// Utilize body-parser for JSON parsing
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.render('index');
+// Handle routes
+app.get('/', (req, res) => {
+  res.render('login'); // Render the login view
 });
 
-app.post("/", (req, res) => {
-  if (req.body.username === "lover" && req.body.password === "lover<3") {
-    res.redirect("/list");
-  } else {
-    res.redirect("/");
-  }
+app.post('/', (req, res) => {
+  res.redirect(`/list`); // Redirect to the '/list' route
 });
 
-app.get("/list", (req, res) => {
-  res.render('list', { users: users.slice(0, 25) });
+app.get('/list', (req, res) => {
+  const itemsToDisplay = 15;
+  const page = parseInt(req.query?.page) || 1;
+  const start = page == 1 ? 0 : (page - 1) * itemsToDisplay - 1;
+  const end = start + itemsToDisplay;
+  const filteredUsers = users.filter((user, idx) => idx > start && idx <= end);
+
+  res.render('list', {
+    title: 'list',
+    users: filteredUsers,
+    itemsToDisplay,
+    page,
+    start,
+    end,
+  });
 });
 
-app.get("/detail/:id", (req, res) => {
-  const id = req.params.id;
-  const user = users.find((user) => user.id == id);
-  res.render('detail', { user: user });
+app.get('/detail/:id', (req, res) => {
+  const user = users.find((user) => {
+    return user.id === parseInt(req.params.id);
+  });
+  res.render('detail', { user });
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+// Employ the apiRouter for managing API requests
+app.use('/api', apiRouter);
+
+// Listen for incoming requests
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
